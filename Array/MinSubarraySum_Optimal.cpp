1fefa605bed538dd1368089314436be811a319fa
Shweta Rajput <shwetarajput0806@gmail.com>
2026-04-15 15:51:20 +0530

    Add MinSubarraySum_Optimal.cpp solution
    Implement solution for Minimum Size Subarray Sum problem.

---
+++ Array/MinSubarraySum_Optimal.cpp
@@ -0,0 +1,37 @@
+//Leetcode Problem -> 209 -> Minimum Size Subarray Sum
+//Time Complexity -> O(n)
+//Space Complexity -> O(1)
+
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+      
+
+
+        int left = 0;
+        int sum = 0;
+        int minLen = INT_MAX;
+
+        for (int right = 0; right < nums.size(); right++) {
+
+            sum += nums[right];
+
+            while (sum >= target) {
+
+                minLen = min(minLen, right - left + 1);
+
+                sum -= nums[left];
+                left++;
+            }
+        }
+
+        if (minLen == INT_MAX)
+            return 0;
+
+        return minLen;
+ 
+        
+ 
+      }
+    
+};
