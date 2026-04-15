36d9d0ec5a1d1f4d92694d29da6f3455518ab07f
Shweta Rajput <shwetarajput0806@gmail.com>
2026-04-15 14:31:42 +0530

    Add optimal solution for 3Sum problem
    Implemented the threeSum function to find all unique triplets in an array that sum to zero.

---
+++ Array/3Sum_optimal.cpp
@@ -0,0 +1,39 @@
+//Leetcode Problem 15------>
+//TimeComplexity :- O(n^2))
+//Space Complexity :- O(1)
+------------------------------------
+  
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j = i+1;
+            int k = n-1;
+            while(j < k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum<0){
+                    j++;
+                }
+                else if(sum >0){
+                    k--;
+
+                }
+                else {
+                    //sum==0;
+                    vector<int>temp ={nums[i],nums[j],nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
