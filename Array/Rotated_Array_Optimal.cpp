502cc119cc517395f1cca29574ecff7e32c794ff
Shweta Rajput <shwetarajput0806@gmail.com>
2026-04-15 14:41:41 +0530

    Implement optimal search in rotated sorted array
    Added an optimal solution for searching in a rotated sorted array using binary search.

---
+++ Array/Rotated_Array_Optimal.cpp
@@ -0,0 +1,37 @@
+//Leetcode Problem 33 -> Search in rotated sorted array
+//Solution -> using binary Search
+//Time complexity -> O(log n)
+//Space complexity ->O(1)
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+           int start =0;
+        int end = nums.size()-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[start]<=nums[mid]){
+                if(nums[start]<=target && target<nums[mid]){
+                end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+        }
+        else{
+            if (nums[mid] < target && target <= nums[end]) {
+                start = mid +1;
+            }
+            else{
+                end = mid -1;
+
+            }
+        }
+      }
+      return -1;
+     
+
+    }
+};
