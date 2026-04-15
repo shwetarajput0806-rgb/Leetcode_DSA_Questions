740f0d0bceb5a48548235b75a619c84f87bc3e76
Shweta Rajput <shwetarajput0806@gmail.com>
2026-04-15 15:47:45 +0530

    Add optimal array rotation solution
    Implement an optimal solution to rotate an array in place using reverse.

---
+++ Array/RotateArray_optimal.cpp
@@ -0,0 +1,22 @@
+//Leetcode -> 189 -> Rotate Array
+//Time Complexity -> O(n)
+//Space Complexity -> O(1)
+
+-------------------------------------------
+  ------------------------------
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;  
+        
+        
+        reverse(nums.begin(), nums.end());
+        
+        reverse(nums.begin(), nums.begin() + k);
+        
+        reverse(nums.begin() + k, nums.end());
+    }
+};
+        
+    
