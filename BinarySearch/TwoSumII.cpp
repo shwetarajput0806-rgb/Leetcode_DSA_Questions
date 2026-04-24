84058ecde5ff3d566e8d44517955b8595ac413be
Shweta Rajput <shwetarajput0806@gmail.com>
2026-04-24 12:40:01 +0530

    Add TwoSumII.cpp for Leetcode Problem 167
    Implement Two Sum II solution for sorted input array.

---
+++ BinarySearch/TwoSumII.cpp
@@ -0,0 +1,23 @@
+//Leetcode Problem -> 167 -> Two Sum II - Input Array Is Sorted
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i=0;
+        int j= numbers.size()-1;
+        while(i<j){
+            int sum = numbers[i]+numbers[j];
+            if(sum == target){
+                return{i+1,j+1};
+            }
+            else if(sum<target){
+                i++;
+            }
+            else if(sum > target){
+                j--;
+            }
+        }
+        return{};
+
+        
+    }
+};
