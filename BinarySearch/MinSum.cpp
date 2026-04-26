1cae79228ffd6ecb28a1b81ecbfa59b1228a583a
Shweta Rajput <shwetarajput0806@gmail.com>
2026-04-26 23:01:22 +0530

    Add MinSum.cpp for Minimum Size Subarray Sum solution
    Implement solution for Leetcode Problem 209 to find the minimum size of a subarray with a sum greater than or equal to a target.

---
+++ BinarySearch/MinSum.cpp
@@ -0,0 +1,35 @@
+//Leetcode Problem -> 209 ->  Minimum Size Subarray Sum
+
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+      
+
+
+        int left = 0;
+        int sum = 0;
+        int minLen = INT_MAX;
+
+        for (int right = 0; right < nums.size(); right++) {
+
+            sum += nums[right];
+
+            while (sum >= target) {
+
+                minLen = min(minLen, right - left + 1);
+
+                sum -= nums[left];
+                left++;
+            }
+        }
+
+        if (minLen == INT_MAX)
+            return 0;
+
+        return minLen;
+ 
+        
+ 
+      }
+    
+};
