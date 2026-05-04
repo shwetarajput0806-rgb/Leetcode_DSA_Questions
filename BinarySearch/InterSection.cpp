962ed0969c5576da3ea7d3937f441c903f6f6c78
Shweta Rajput <shwetarajput0806@gmail.com>
2026-05-04 21:20:30 +0530

    Add Intersection of Two Arrays solution
    Implement intersection of two arrays using a hashmap to track frequencies.

---
+++ BinarySearch/InterSection.cpp
@@ -0,0 +1,28 @@
+//Leetcode PRoblem -> 349 -> Intersection of Two Arrays
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+             unordered_map<int,int> freq;
+
+        // Step 1: Count nums1
+        for(int num : nums1){
+            freq[num]++;
+        }
+
+        vector<int> ans;
+
+        // Step 2: Check nums2
+        for(int num : nums2){
+
+            if(freq[num] > 0){
+
+                ans.push_back(num);
+                freq[num] = 0; // unique banane ke liye
+            }
+        }
+
+        return ans;
+        
+    }
+};
