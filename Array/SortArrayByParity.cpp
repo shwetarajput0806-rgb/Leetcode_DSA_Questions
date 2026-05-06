67d8a4736589ad80b65101dab5bf5d49899271ad
Shweta Rajput <shwetarajput0806@gmail.com>
2026-05-06 18:21:53 +0530

    Add SortArrayByParity class and method
    Implement sorting of array by parity using two pointers.

---
+++ Array/SortArrayByParity.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int start=0;
+        int end = nums.size()-1;
+        while(start<end){
+            if(nums[start]%2==0){
+                start++;
+            }
+            else if(nums[end]%2 != 0){
+                end--;
+            }
+            else{
+                swap(nums[start],nums[end]);
+                start++;
+                end--;
+            }
+        }
+        return nums;
+        
+    }
+};
