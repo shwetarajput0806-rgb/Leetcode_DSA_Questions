72a7b4e32f8aa070beb7f7437e95e3be8a008713
Shweta Rajput <shwetarajput0806@gmail.com>
2026-05-06 18:04:39 +0530

    Add MaxConsecutive class for LeetCode problem 485
    Implement solution for finding maximum consecutive ones in an array.

---
+++ Array/MaxConsecutive.cpp
@@ -0,0 +1,21 @@
+//LeetCode Problem -> 485 -> Max Consecutive Ones
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+    int count = 0;
+    int maxi = 0;
+    
+    for(int i = 0; i < nums.size(); i++) {
+        if(nums[i] == 1) {
+            count++;
+            maxi = max(maxi, count);
+        } else {
+            count = 0; // reset
+        }
+    }
+    
+    return maxi;
+}
+        
+    
+};
