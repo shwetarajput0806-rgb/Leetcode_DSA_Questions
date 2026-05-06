d81ada822635fc7dce30c369672343a6a383ee55
Shweta Rajput <shwetarajput0806@gmail.com>
2026-05-06 18:20:38 +0530

    Add solution for Boats to Save People problem
    Implement solution for the 'Boats to Save People' problem by sorting the people and using a two-pointer approach to determine the minimum number of boats needed.

---
+++ Array/BoatsToSavePeoples.cpp
@@ -0,0 +1,26 @@
+//LeeetCode Problem -> 881 ->  Boats to Save People
+
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int i=0;
+        int j= people.size()-1;
+        int boat = 0;
+        while(i<=j){
+            if(people[i]+people[j]<= limit){
+                i++;
+                j--;
+                boat++;
+            }
+            else{ //if(people[i]+people[j]>limit)
+                j--;
+                boat++;
+
+            }
+        }
+        return boat;
+
+        
+    }
+};
