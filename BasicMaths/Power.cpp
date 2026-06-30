898996e19a4eda52b92bfeada3d1c8fc66504e0f
Shweta Rajput <shwetarajput0806@gmail.com>
2026-06-30 22:06:45 +0530

    Add myPow function for power calculation
    Implement myPow function to calculate x raised to the power n using iterative exponentiation.

---
+++ BasicMaths/Power.cpp
@@ -0,0 +1,26 @@
+//Leetcode Problem -> 50  ->  Pow(x, n)
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;  // handle INT_MIN safely
+        
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        
+        double result = 1;
+        
+        while (N > 0) {
+            if (N % 2 == 1) {   // if odd
+                result *= x;
+            }
+            x *= x;   // square the base
+            N /= 2;   // reduce power
+        }
+        
+        return result;
+    }
+};
+      
