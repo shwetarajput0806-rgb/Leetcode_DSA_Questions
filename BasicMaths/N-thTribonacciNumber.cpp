fcd092858a56c6db5a12612bb794ed8b11914c69
Shweta Rajput <shwetarajput0806@gmail.com>
2026-07-17 22:48:23 +0530

    Create  N-thTribonacciNumber.cpp

---
+++ BasicMaths/N-thTribonacciNumber.cpp
@@ -0,0 +1,28 @@
+//Leetcode Problem ->1137  ->  N-th Tribonacci Number
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==0){
+            return 0;
+        }
+        else if(n==1 || n==2){
+            return 1;
+        }    
+
+        int first =0;
+        int second =1;
+        int third =1;
+        int temp;
+        for(int i=3;i<=n;i++ ){
+            temp =first;
+            temp = first +second +third;
+            first =second;
+            second =third;
+            third =temp;
+            
+        }
+        return temp;
+        
+    }
+};
